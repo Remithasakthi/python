4e701492523f946982543575293dd80a92299517
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2018-09-25 16:28:41 +0530

    Update alphabet or not.py

--- alphabet or not.py
+++ alphabet or not.py
@@ -1,8 +1,5 @@
-ch = input("Enter any character: ");
-if ch == '0':
-    exit();
-else:
-    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print(ch, "is an alphabet.");
-    else:
-    	print(ch, "is not an alphabet.");
+ch=input("Enter a character:")
+if((ch>='a'=ch<='z')or(ch>='A')andch<='z')):
+    print(ch,"is an alphabet")
+ else:
+    print(ch,"is not an alphabet")
