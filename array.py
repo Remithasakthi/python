00e7f88309a6b2af89709c6fcaa423c7d48b5711
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2018-09-28 17:46:33 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,9 @@
+def main():
+    numbers=eval(input("give me an array of numbers:"))
+    smallest=numbers[0]
+    for i in range(o,len(numbers),1):
+        if(numbers[i]<smallest):
+            smallest=numbers[i]
+            print("The smallest number is:",smallest)
+main()           
+    
