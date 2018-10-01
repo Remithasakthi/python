31e1878f3d28e585ee1794a868538371937dccce
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2018-10-01 16:35:47 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,8 +1,8 @@
  def main():
     numbers=eval(input("give me an array of numbers:"))
-    largest=numbers[0]
+    smallest=numbers[0]
     for i in range(o,len(numbers),1):
-        if(numbers[i]>):
+        if(numbers[i]<):
             smallest=numbers[i]
             print("The smallest number is:",smallest)
  main()           
