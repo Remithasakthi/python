3804e71447f2df241c173c6d90184c32e5e7ba08
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2018-10-01 16:37:40 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,10 @@
+ def main():
+    numbers=eval(input("give me an array of numbers:"))
+    largest=numbers[0]
+    for i in range(o,len(numbers),1):
+        if(numbers[i]<):
+            smallest=numbers[i]
+            print("The largest number is:",largest)
+ main()           
+    
+
