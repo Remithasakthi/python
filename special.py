8f1a7554f7e900105c7f14e2fa7b6a5295578d4c
Remithasakthi <43408064+Remithasakthi@users.noreply.github.com>
2019-02-05 15:16:19 +0530

    Create special.py

---
+++ special.py
@@ -0,0 +1,11 @@
+ String=raw_input("enter string:")
+ Count1=0
+ Count2=0
+ for i in string:
+      if(i.isdigit()):
+            count1=count1+1
+      count2=count2+1
+ Print("The number of digits is:")
+ Print(count1)
+ Print("The number of characters is:")
+ Print(count2)
